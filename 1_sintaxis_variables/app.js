8857754a46b9099923a5185bad405f72874a8e57
Yuranyy14 <cardenasuribeyurany@gmail.com>
2025-07-31 12:26:05 -0500

    repositorio js versión 1

---
+++ 1_sintaxis_variables/app.js
@@ -0,0 +1,26 @@
+//El alert muestra una ventana emergente en el navegador
+//alert ("Hola Mundo");
+//El promp una caja para ingresar informacion desde el navegador 
+//prompt("Dime tu nombre")
+
+//Variable declarada con let 
+let personaje = "Perrito"
+//Los valores numericos van sin comillas
+let puntaje = 0
+let shots = 3
+    recarga = 2 
+
+console.log("Total balas:",shots + recarga) 
+
+//El usuario capturo 4 patos
+puntaje += 4
+//El usuario gasto 2 balas
+shots -= 2
+//El ususario recargo su arma 
+recarga += 1
+recarga++ //++ Permite sumar de a uno 
+recarga++
+
+console.log("Puntaje:",puntaje)
+console.log("Shots:",shots)
+console.log("Recarga:",recarga)
---
+++ 2_Operadores_comparacion_logicos/app.js
@@ -0,0 +1,28 @@
+//Bolean(...)
+
+// console.log('"5" == "5"', "5" == "5") //true
+// console.log('"5" === "5"', "5" === "5") //true
+// console.log('5 === "5"', 5 === "5") //false
+// console.log('5 == "5"', 5 == "5") //true
+// console.log('5 === (3+2)', "5" === (3+2))//false
+// console.log('5 === 5', 5 === 5)//true
+
+// //Concatenar (+)
+// console.log(5+"Tres") //5Tres
+// console.log(5+ Number("3") ) //8
+// console.log( 5 * "3") // 15
+// console.log(5+3) //8
+
+// //Pida dos numeros al ususario y sumelos 
+// let num1 = Number(prompt('Dame num 1')) //"5"
+// let num2 = Number(prompt('Dame num 2'))//"7"
+// let resultado = num1 + num2
+// alert("El resultado de tu suma es " + resultado)
+
+let valorA = Number(prompt('Dame un valor A'))
+let valorB = Number(prompt('Dame un valor B'))
+let resultado = valorA + valorB
+let resultado = valorA - valorB
+let resultado = valorA * valorB
+let resultado = valorA / valorB
+alert("El resultado de los valores son " + resultado)0 
